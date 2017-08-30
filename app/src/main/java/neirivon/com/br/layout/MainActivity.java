25647d28d4ac37155edf370edabe339055ae01ed
neirivon <neirivon@gmail.com>
2017-08-29 21:08:20 -0300

    Initial commit

---
+++ app/src/main/java/neirivon/com/br/layout/MainActivity.java
@@ -0,0 +1,13 @@
+package neirivon.com.br.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_calculadora);
+    }
+}
